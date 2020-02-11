7072a51caef181aa2479ebadc8ba083a63dff7f7
Frodo4500 <31665995+Frodo4500@users.noreply.github.com>
2020-02-11 19:30:52 +0300

    Add files via upload

--- Task_4/src/Main.java
+++ Task_4/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         int oneArray[] = {4,3,12,36,45,9,66,12,3};
         int twoArray[][] = {{1,1,1},{2,2,2},{3,3,3}};
-        System.out.println(ArrayConverter.loopMaximalElementInOneDimensionalArray(oneArray));
+        System.out.println(ArrayTools.loopMaximalElementInOneDimensionalArray(oneArray));
 
-        System.out.println(ArrayConverter.swapColumns(twoArray));
-        System.out.println(ArrayConverter.swapLines(twoArray));
+        System.out.println(ArrayTools.swapColumns(twoArray));
+        System.out.println(ArrayTools.swapLines(twoArray));
     }
 }
