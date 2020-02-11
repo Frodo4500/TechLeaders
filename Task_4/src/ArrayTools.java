8e0d2c1ac5597b5a93790ecf928aacc63b22a81b
Frodo4500 <31665995+Frodo4500@users.noreply.github.com>
2020-02-11 19:50:01 +0300

    Add files via upload

--- Task_4/src/ArrayTools.java
+++ Task_4/src/ArrayTools.java
@@ -2,20 +2,20 @@ public class ArrayTools {
 
     public static int loopMaximalElementInOneDimensionalArray(int oneArray[]) {
         int maxElement = 0;
-        for (int i=0 ; i<oneArray.length;i++){
-            if (oneArray[i] > maxElement) { maxElement = oneArray[i]; };
-        }
-//        for (int i : oneArray){             // почему не работает
+//        for (int i=0 ; i<oneArray.length;i++){
 //            if (oneArray[i] > maxElement) { maxElement = oneArray[i]; };
 //        }
+        for (int i : oneArray){             // почему не работает
+            if (oneArray[i] > maxElement) { maxElement = oneArray[i]; };
+        }
         return maxElement;
     }
 
     public static String swapLines(int twoArray[][]) {
         int i = 0;
         int j = 2;
+        int[][] arrayZ = {{0, 0}, {0, 0}};
         for (int k = 0; k < twoArray.length; k++) {
-            int[][] arrayZ = {{0, 0}, {0, 0}};
             arrayZ[0][0] = twoArray[i][k];
             twoArray[i][k] = twoArray[j][k];
             twoArray[j][k] = arrayZ[0][0];
@@ -26,8 +26,8 @@ public class ArrayTools {
     public static String swapColumns(int twoArray[][]){
         int i = 0;
         int j = 2;
+        int[][] arrayZ = {{0, 0}, {0, 0}};
         for (int k = 0; k < twoArray.length; k++) {
-            int[][] arrayZ = {{0, 0}, {0, 0}};
             arrayZ[0][0] = twoArray[k][i];
             twoArray[k][i] = twoArray[k][j];
             twoArray[k][j] = arrayZ[0][0];
@@ -46,8 +46,4 @@ public class ArrayTools {
         }
         return sumDiagonal;
     }
-
 }
-
-//        главная диагональ
-//                i=j
